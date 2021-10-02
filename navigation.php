041f3aa48b36c9e9d9dc5b4165cc0eca3bb3741d
Vishnu Dhanda <vishnu.dhanda@ucalgary.ca>
2021-10-03 00:06:01 +0300

    desktop site

---
+++ navigation.php
@@ -0,0 +1,48 @@
+<header>
+<link rel="shortcut icon" href="website_images/favicon.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="desktopstyles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="desktopstyles.css">
+</header>
+
+<! --Navigation Begins -->
+
+<nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top">
+    <div class="container">
+      <a href="#header" class="navbar-brand">Pickaxe Miner</a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navmenu"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navmenu">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="/download.php" class="nav-link">Download</a>
+          </li>
+          <li class="nav-item">
+            <span hidden> <a href="https://pickaxeminer.us/tiktok_stats.php" class="nav-link">TikTok Stats</a></span>
+          </li>
+          <li class="nav-item">
+            <a href="index.html#features" class="nav-link">Features</a>
+          </li>
+          <li class="nav-item">
+            <a href="index.html#FAQ" class="nav-link">FAQ</a>
+          </li>
+          <li class="nav-item">
+            <a href="index.html#contactus" class="nav-link">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
